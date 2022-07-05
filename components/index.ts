a06499f3d0b57a8d2778ef1782c893645a6fdb7a
rnwathon <rakhmatnashrul@gmail.com>
2022-07-05 22:12:45 +0700

    feat: export custom components

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Buttons';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as MoviesItem } from './Movies/MoviesItem';
+export { default as MoviesRow } from './Movies/MoviesRow';
+export { default as Navbar } from './Navbar';
